72014ba331852c3fbaa55c77f5ed61a6778d78d8
TangRuolin <190935423@qq.com>
2018-12-12 16:11:41 +0800

    C#版本

---
+++ Assets/Game/Scripts/Manager/Event/EventID.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventID {
+
+    public enum UIEvent
+    {
+        
+    }
+
+    public enum SceneEvent
+    {
+        SynLoad = 1,
+        AsynLoad = 2,
+    }
+
+
+
+}
