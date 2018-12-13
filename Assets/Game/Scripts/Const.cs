f171c28b061ccfd46274d666ac34ac40649b8be4
TangRuolin <190935423@qq.com>
2018-12-13 19:11:40 +0800

    2018.12.13  19:11

---
+++ Assets/Game/Scripts/Const.cs
@@ -0,0 +1,9 @@
+﻿///整个游戏所需要的常量，都存放在这个类里
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class Const {
+
+    public const float logoTitleSpe = 1;
+}
--- Assets/Game/Scripts/Manager/UIMgr.cs
+++ Assets/Game/Scripts/Manager/UIMgr.cs
@@ -16,4 +16,5 @@ public class UIMgr{
             return _instance;
         }
     }
+   
 }
